348399c9fec3b36c9fb20b0884c4cc6a85651f1c
Dominik Zogg <dominik.zogg@gmail.com>
2014-12-05 17:28:08 +0100

    add a new resizer

---
+++ Resizer/ImagickNoCropThumbnailResizer.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Ibrows\SonataMediaResizerBundle\Resizer;
+
+use Gaufrette\File;
+use Imagine\Image\Box;
+use Sonata\MediaBundle\Metadata\MetadataBuilderInterface;
+use Sonata\MediaBundle\Model\MediaInterface;
+use Sonata\MediaBundle\Resizer\ResizerInterface;
+
+class ImagickNoCropThumbnailResizer implements ResizerInterface
+{
+    /**
+     * @var MetadataBuilderInterface
+     */
+    protected $metadata;
+
+    /**
+     * @var string
+     */
+    protected $mode;
+
+    /**
+     * @param MetadataBuilderInterface $metadata
+     */
+    public function __construct(MetadataBuilderInterface $metadata)
+    {
+        $this->metadata = $metadata;
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param File           $in
+     * @param File           $out
+     * @param string         $format
+     * @param array          $settings
+     *
+     * @return void
+     */
+    public function resize(MediaInterface $media, File $in, File $out, $format, array $settings)
+    {
+        if(!extension_loaded('imagick')){
+            $out->setContent($in->getContent(), $this->metadata->get($media, $out->getName()));
+            return;
+        }
+
+        $this->validateSettings($media, $settings);
+
+        list($width, $height) = $this->getDimensions($media, $settings);
+
+        $image = new \Imagick();
+        $image->newImage($settings['width'], $settings['height'], new \ImagickPixel('none'));
+        $image->setImageFormat('png');
+
+        $origImage = new \Imagick();
+        $origImage->readimageblob($in->getContent());
+        $origImage->resizeImage($width, $height, \Imagick::FILTER_LANCZOS,1);
+
+        $image->compositeImage($origImage, \Imagick::COMPOSITE_OVER, ($settings['width'] - $width) / 2 , ($settings['height'] - $height) / 2);
+
+        $out->setContent($image, $this->metadata->get($media, $out->getName()));
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param array $settings
+     * @return Box
+     */
+    public function getBox(MediaInterface $media, array $settings)
+    {
+        return new Box($settings['width'], $settings['height']);
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param array $settings
+     * @return array
+     * @throws \RuntimeException
+     */
+    protected function getDimensions(MediaInterface $media, array $settings)
+    {
+        $this->validateSettings($media, $settings);
+
+        $origWidth = $media->getWidth();
+        $origHeight = $media->getHeight();
+        $origRatio = $origWidth / $origHeight;
+
+        $wishedWidth = $settings['width'];
+        $wishedHeight = $settings['height'];
+        $wishedRatio = $wishedWidth / $wishedHeight;
+
+        // orig image is wider than the wished
+        if($origRatio > $wishedRatio) {
+            if($origWidth > $wishedWidth) {
+                $width = $wishedWidth;
+            } else {
+                $width = $origWidth;
+            }
+            $height = $width / $origRatio;
+        } else {
+            if($origHeight > $wishedHeight) {
+                $height = $wishedHeight;
+            } else {
+                $height = $origHeight;
+            }
+            $width = $origRatio * $height;
+        }
+
+        return array($width, $height);
+    }
+
+    /**
+     * @param MediaInterface $media
+     * @param array          $settings
+     */
+    protected function validateSettings(MediaInterface $media, array &$settings)
+    {
+        if (!$settings['width']) {
+            throw new \RuntimeException(sprintf('Width parameter is missing in context "%s" for provider "%s"', $media->getContext(), $media->getProviderName()));
+        }
+
+        if (!$settings['height']) {
+            $settings['height'] = $settings['width'];
+        }
+    }
+}
